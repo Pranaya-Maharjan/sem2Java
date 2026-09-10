2aab7305c966e4c4963554fb1a8dd8bf699d11df
Pranaya Maharjan <bucketspongy@.com>
2026-09-10 10:12:23 +0545

    Updated notes and programs

---
+++ Java/lab8/CharacterStreamExample.java
@@ -0,0 +1,12 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+class CharacterStreamExample{
+	public static voidd main(String args[]){
+		String file_path = "subject.txt";
+		File f = new File(file_path);
+	}
