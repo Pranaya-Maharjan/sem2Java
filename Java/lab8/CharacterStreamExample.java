a3f2d866655f647cbabe73fd72ad3cef45315e1a
Pranaya Maharjan <bucketspongy@.com>
2026-09-10 11:04:38 +0545

    Added lab8

--- Java/lab8/CharacterStreamExample.java
+++ Java/lab8/CharacterStreamExample.java
@@ -6,7 +6,38 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 class CharacterStreamExample{
-	public static voidd main(String args[]){
-		String file_path = "subject.txt";
-		File f = new File(file_path);
+	public static void main(String args[]){
+		String file_path = "/home/spongybucket/Documents/sem2Java/Java/lab8/subject.txt";
+		FileReader reader = null;
+		try{
+                        File f = new File(file_path);
+		        reader = new FileReader(f);
+			for(int i = 0; i < f.length(); i++){
+				int data = reader.read();
+				if(data == -1){
+					break;
+				}
+				System.out.print((char)data);
+			}
+		File fnew = new File("/home/spongybucket/Documents/sem2Java/Java/lab8/subject_new.txt");
+		String data = "lab 8: Character Stream Example. \nThis is a new file created using FIleWriter class.\n";
+		FileWriter writer = new FileWriter(fnew);
+		writer.write(data);
+		writer.close();
+		System.out.println("\nData written to the file successfully.");
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(reader != null){
+					reader.close();
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
 	}
+}
+  
